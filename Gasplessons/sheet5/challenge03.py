79374adeff88f2cac8d872b46a7725e3abbb8e1c
Blu Medina <blu.medina.biz@gmail.com>
2023-10-07 13:12:04 -0400

    finished challenge03

--- Gasplessons/sheet5/challenge03.py
+++ Gasplessons/sheet5/challenge03.py
@@ -6,6 +6,11 @@ ball_x = 5
 ball_y = 5
 c = Circle((ball_x, ball_y),5)
 
- 
+while ball_x < 630 and ball_y < 470:
+    ball_x = ball_x + 4
+    ball_y = ball_y + 3
+    move_to(c, (ball_x, ball_y))
+    time.sleep(0.02)
+
 
 end_graphics()
