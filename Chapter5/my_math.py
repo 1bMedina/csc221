d876e4d6c4369b2967285fda0292eb43b7ecbc08
Blu Medina <blu.medina.biz@gmail.com>
2023-10-26 20:13:15 -0400

    did some math

---
+++ Chapter5/my_math.py
@@ -0,0 +1,16 @@
+import math
+#1. ulp
+#2. frexp
+#3. lgamma
+#4. perm
+#5. hypot
+#6. isfinite
+#7. isqrt
+#8. nextafter
+#9. pi
+#10. floor
+
+print(math.pi)
+print(math.frexp(2))
+print(math.isqrt(16))
+print(math.hypot(3))
